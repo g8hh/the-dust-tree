ca8b2303be6f685f902bdcc40cc8732ef62e3d38
chipsams <77809761+chipsams@users.noreply.github.com>
2021-12-12 16:21:35 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-//https://raw.githack.com/chipsams/The-Modding-Tree/master/
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+//https://optimistic-snyder-de15e4.netlify.app/
+addLayer("cr", {
+    name: "crafting", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -8,12 +8,12 @@ addLayer("p", {
 		points: new Decimal(0),
     }},
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
+    resource: "gathered dust", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -25,7 +25,7 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    upgrades: {
+    buyable: {
       11: {
         title: "hi"
       }
