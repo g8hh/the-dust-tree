245f5486fa24d7c29793abf89c7a14ccf7b158a1
chipsams <77809761+chipsams@users.noreply.github.com>
2021-12-13 06:32:05 +0000

    added the craft system

--- js/layers.js
+++ js/layers.js
@@ -1,40 +1,112 @@
 //https://optimistic-snyder-de15e4.netlify.app/
-addLayer("cr", {
+let data={
     name: "crafting", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "CR", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
+      unlocked: true,
+      points: new Decimal(0),
+      items: {
+        packed_dust: new Decimal(0)
+      },
+      selected: "",
+      resources: {
+        101:"dust",
+        102:"compressed dust",
+        103:"dust bricks",
+        104:"dust shards"
+      },
     }},
     color: "#4BDC13",
-    requires: new Decimal(1), // Can be a function that takes requirement increases into account
-    resource: "gathered dust", // Name of prestige currency
-    baseResource: "dust", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
+    type: "none",
     row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
+    hotkeys: [],
     buyables: {
       11: {
-        cost(x) { return new Decimal(1).mul(x) },
-        display() { return "Blah" },
+        cost(x) { return new Decimal(5) },
+        display() { return `packed dust x${player.cr.items.packed_dust}\nuse ${this.cost()} dust to produce another.` },
         canAfford() { return player[this.layer].points.gte(this.cost()) },
         buy() {
             player[this.layer].points = player[this.layer].points.sub(this.cost())
-            setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+            let i=player.cr.items
+            i.packed_dust=i.packed_dust.add(1)
         },
       },
     },
+    clickables: {
+      rows:9,
+      cols:9,
+      11: {
+        canClick() {return true},
+        onClick() {
+          if (player.cr.selected){
+            setClickableState(this.layer,this.id,player.cr.selected)
+            player.cr.selected=""
+          }
+        },
+        display() {return getClickableState(this.layer,this.id)}
+      },
+      12: {
+        canClick() {return true},
+        onClick() {
+          if (player.cr.selected){
+            setClickableState(this.layer,this.id,player.cr.selected)
+            player.cr.selected=""
+          }
+        },
+        display() {return getClickableState(this.layer,this.id)}
+      },
+      13: {
+        canClick() {return true},
+        onClick() {},
+        display() {return `selected: ${player.cr.selected}`}
+      },
+    },
+    tabFormat: {
+      "Main tab": {
+          content: ["buyables"],
+      },
+      "Other tab": {
+          content: ["clickables","grid"],
+      },
+    },
+    grid: {
+      rows: 7,
+      cols: 6,
+      getStartData(id) {
+          return {amount: new Decimal(0)}
+      },
+      getUnlocked(id) { // Default
+          return true
+      },
+      getCanClick(data, id) {
+          return true
+      },
+      getStyle(data, id) {
+          return {'background-color': data.amount.gt(0)?(player.cr.selected==player.cr.resources[id]?"#AAAAAA":"#888888"):"#222222"}
+      },
+      onClick(data, id) { // Don't forget onHold
+          if (player.cr.resources[id]){
+            data.amount=data.amount.add(1)
+            select_resource(player.cr.resources[id])
+          }
+      },
+      getTitle(data, id) {
+          return player.cr.resources[id]||"none"
+      },
+      getDisplay(data, id) {
+          return data.amount
+      },
+    },
     layerShown(){return true}
-})
+}
+
+function select_resource(button){
+  if (player.cr.selected==button){
+    player.cr.selected=""
+  }else{
+    player.cr.selected=button
+  }
+}
+
+addLayer("cr", data)
